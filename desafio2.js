c470adbcd43c5158636c87f3fbb8eed78d549aa8
anacarlafp <69883772+anacarlafp@users.noreply.github.com>
2021-07-25 18:16:46 -0300

    Update desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,21 +1,26 @@
-const tBody = document.getElementById('estoque.html');
-
+const tBody = document.getElementById('estoque.html'),
 
   var seu_nome = document.getElementById("Seu nome").value;
   var email = document.getElementById("E-mail").value;
   var CPF = document.getElementById("CPF").value; 
-  var nome_produto = document.getElementById("E-mail").value;
-  var quantidade = document.getElementById("CPF").value;
 
   var info = {
     seu_nome,
     email,
     CPF,
+
+    const estoque = JSON.parse(localStorage.getItem('estoque'),
+  }
+
+const tBody = document.getElementById('estoque2.html'),
+
+  var nome_produto = document.getElementById("Nome do Produto").value;
+  var quantidade = document.getElementById("Quantidade").value;
+
+  var info = {
     nome_produto,
     quantidade,
-}
-
-function buscarConteudo() {
-  const arr = JSON.parse(localStorage.getItem('estoque2'));
+    
+    const estoque2 = JSON.parse(localStorage.getItem('estoque2'),
 
 }
